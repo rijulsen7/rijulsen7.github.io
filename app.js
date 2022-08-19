5d727bfea913409221bdc1261b1e3b8025735a81
Rijul sen <68010035+rijulsen7@users.noreply.github.com>
2022-08-19 14:25:25 +0530

    preloader

---
+++ app.js
@@ -0,0 +1,9 @@
+// pre loader start
+ function loader() {
+     document.querySelector('.loader-container').classList.add('fade-out');
+ }
+ function fadeOut() {
+     setInterval(loader, 500);
+ }
+ window.onload = fadeOut;
+// pre loader end
